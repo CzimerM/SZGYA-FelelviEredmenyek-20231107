97fb90b65c2469e942a7ee9d3d723eb6bc58d378
Airamek <vegkeppnemairamek@gmail.com>
2023-11-07 13:15:54 +0100

    Add project files.

---
+++ SZGYA-FelelviEredmenyek-20231107/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SZGYA_FelelviEredmenyek_20231107
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Tanulo> tanulok = new List<Tanulo>();
+            StreamReader sr = new StreamReader("../../../src/tanulok.txt", Encoding.UTF8);
+            _ = sr.ReadLine();
+            for (int i = 1; !sr.EndOfStream; i++)
+            {
+                string sor = sr.ReadLine();
+                if (i % 2 == 0)
+                {
+                    tanulok.Add(new Tanulo(sor));
+                }
+            }
+
+            Console.WriteLine($"1.feladat: osztályátlag: {osszAtlag(tanulok)}");
+            foreach (var x in tantargyAtlag(tanulok))
+            {
+                Console.WriteLine($"\t{x.Key}: {x.Value}");
+            }
+
+        }
+
+        static double osszAtlag(List<Tanulo> tanulok)
+        {
+            return tanulok.Average(t => t.Atlag);
+        }
+
+        static Dictionary<string, double> tantargyAtlag(List<Tanulo> tanulok)
+        {
+            Dictionary<string, double> atlagok = new Dictionary<string, double>();
+            foreach (var x in tanulok.First().Jegyek.Keys)
+            {
+                atlagok[x] = tanulok.Average(t => t.Jegyek[x]);
+            }
+            return atlagok;
+        }
+    }
+}
---
+++ SZGYA-FelelviEredmenyek-20231107/Tanulo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZGYA_FelelviEredmenyek_20231107
+{
+    class Tanulo
+    {
+        public string Nev { get; set; }
+        public long Azonosito { get; set; }
+        public Dictionary<string, int> Jegyek { get; set; }
+
+        public double Atlag => this.Jegyek.Values.Average();
+
+        public Tanulo(string sor)
+        {
+            string[] adatok = sor.Split("\t");
+            this.Nev = adatok[0];
+            this.Azonosito = long.Parse(adatok[1]);
+            this.Jegyek = new Dictionary<string, int>();
+            this.Jegyek["halozatok1"] = int.Parse(adatok[2]);
+            this.Jegyek["halozatok2"] = int.Parse(adatok[3]);
+            this.Jegyek["programozas"] = int.Parse(adatok[4]);
+            this.Jegyek["programozas_gyak"] = int.Parse(adatok[5]);
+            this.Jegyek["angol"] = int.Parse(adatok[6]);
+            this.Jegyek["magyar"] = int.Parse(adatok[7]);
+            this.Jegyek["matek"] = int.Parse(adatok[8]);
+            this.Jegyek["tori"] = int.Parse(adatok[9]);
+        }
+    }
+}
